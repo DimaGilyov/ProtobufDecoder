29189ff0c9a8de54dde3d1087fa3885f23a68c21
DimaG <dimagilev91@gmail.com>
2023-06-23 00:44:25 +0600

    version 1

--- ProtobufDecoder/ProtobufDecoder.cs
+++ ProtobufDecoder/ProtobufDecoder.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ProtobufDecoder
 {
+    /// <summary>
+    /// https://developers.google.com/protocol-buffers/docs/encoding
+    /// </summary>
     internal class ProtobufDecoder
     {
         private readonly Dictionary<byte, string> fieldTypes = new Dictionary<byte, string>();
